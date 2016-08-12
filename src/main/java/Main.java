c51c61a2af841265457572f4376906fa672f9a1f
boolyahoo <boolyahoo@gmail.com>
2016-08-12 14:09:31 +0800

    official demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.*;
@@ -6,7 +5,6 @@ import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
-import org.junit.Before;
 
 import java.io.IOException;
 
@@ -15,95 +13,78 @@ import java.io.IOException;
  * Created by xcoder on 8/12/16.
  */
 public class Main {
-    Configuration conf;
-    Connection conn;
+    private static final String TABLE_NAME = "MY_TABLE_NAME_TOO";
+    private static final String CF_DEFAULT = "DEFAULT_COLUMN_FAMILY";
 
-    @Before
-    public void init() {
-        conf = HBaseConfiguration.create();
-        conf.set("hbase.zookeeper.property.clientPort", "2181");
-        conf.set("hbase.zookeeper.quorum", "hadoop,hadoop1,hadoop2");
+    public static void main(String... args) throws IOException {
+        Configuration config = HBaseConfiguration.create();
+        //Add any necessary configuration files (hbase-site.xml, core-site.xml)
+        config.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"));
+        config.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"));
+        createSchemaTables(config);
+        modifySchema(config);
+    }
+
+    public static void createOrOverwrite(Admin admin, HTableDescriptor table) throws IOException {
+        if (admin.tableExists(table.getTableName())) {
+            admin.disableTable(table.getTableName());
+            admin.deleteTable(table.getTableName());
+        }
+        admin.createTable(table);
+    }
+
+    public static void createSchemaTables(Configuration config) throws IOException {
         try {
-            conn = ConnectionFactory.createConnection(conf);
-        } catch (IOException e) {
+            Connection connection = ConnectionFactory.createConnection(config);
+            Admin admin = connection.getAdmin();
+            HTableDescriptor table = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
+            table.addFamily(new HColumnDescriptor(CF_DEFAULT).setCompressionType(Algorithm.SNAPPY));
+
+            System.out.print("Creating table. ");
+            createOrOverwrite(admin, table);
+            System.out.println(" Done.");
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @
-    public void createTable() throws IOException {
-
-        HBaseAdmin admin = (HBaseAdmin) conn.getAdmin();
-        HTableDescriptor htd = new HTableDescriptor(TableName.valueOf("people"));
-        HColumnDescriptor htd_info = new HColumnDescriptor("info");
-        htd.addFamily(htd_info);
-        htd.addFamily(new HColumnDescriptor("data"));
-        htd_info.setMaxVersions(3);
-
-        admin.createTable(htd);
-        admin.close();
-    }
-
-    public void testPut() throws IOException {
-        HTableDescriptor htd = new HTableDescriptor(TableName.valueOf("people"));
-        HTable table = (HTable) conn.getTable(TableName.valueOf("people"));
-
-        Put put = new Put(Bytes.toBytes("rk0001"));
-        put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("name"),
-                Bytes.toBytes("zhangsan"));
-        put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("age"),
-                Bytes.toBytes("25"));
-        put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("money"),
-                Bytes.toBytes("10w"));
-        table.put(put);
-    }
-
-    public void testPutAll() throws IOException {
-        // HTablePool pool =
-
-        HTable table = (HTable) conn.getTable(TableName.valueOf("people"));
-
-        List<Put> puts = new ArrayList<Put>(10000);
-        for (int i = 1; i <= 100001; i++) {
-            Put put = new Put(Bytes.toBytes("rk" + i));
-            put.addImmutable(Bytes.toBytes("info"), Bytes.toBytes("money"),
-                    Bytes.toBytes("" + i));
-            puts.add(put);
-            if (i % 10000 == 0) {
-                table.put(puts);
-                puts = new ArrayList<Put>(10000);
+    public static void modifySchema(Configuration config) throws IOException {
+        try {
+            Connection connection = ConnectionFactory.createConnection(config);
+            Admin admin = connection.getAdmin();
+            TableName tableName = TableName.valueOf(TABLE_NAME);
+            if (!admin.tableExists(tableName)) {
+                System.out.println("Table does not exist.");
+                System.exit(-1);
             }
+
+            HTableDescriptor table = new HTableDescriptor(tableName);
+
+            // Update existing table
+            HColumnDescriptor newColumn = new HColumnDescriptor("NEWCF");
+            newColumn.setCompactionCompressionType(Algorithm.GZ);
+            newColumn.setMaxVersions(HConstants.ALL_VERSIONS);
+            admin.addColumn(tableName, newColumn);
+
+            // Update existing column family
+            HColumnDescriptor existingColumn = new HColumnDescriptor(CF_DEFAULT);
+            existingColumn.setCompactionCompressionType(Algorithm.GZ);
+            existingColumn.setMaxVersions(HConstants.ALL_VERSIONS);
+            table.modifyFamily(existingColumn);
+            admin.modifyTable(tableName, table);
+
+            // Disable an existing table
+            admin.disableTable(tableName);
+
+            // Delete an existing column family
+            admin.deleteColumn(tableName, CF_DEFAULT.getBytes("UTF-8"));
+
+            // Delete a table (Need to be disabled first)
+            admin.deleteTable(tableName);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public void testGet() throws IOException {
-
-        HTable table = (HTable) conn.getTable(TableName.valueOf("people"));
-        Get get = new Get(Bytes.toBytes("rk9999"));
-        Result result = table.get(get);
-        String str = Bytes.toString(result.getValue(Bytes.toBytes("info"),
-                Bytes.toBytes("money")));
-        System.out.println(str);
-        table.close();
-    }
-
-    public void testDelete() throws IOException {
-        HTable table = (HTable) conn.getTable(TableName.valueOf("people"));
-        Delete delete = new Delete(Bytes.toBytes("rk9999"));
-        table.delete(delete);
-        table.close();
-    }
-
-    public void testScan() throws IOException {
-        HTable table = (HTable) conn.getTable(TableName.valueOf("people"));
-        Scan scan = new Scan(Bytes.toBytes("rk29990"), Bytes.toBytes("rk30000"));
-        ResultScanner resultScaner = table.getScanner(scan);
-        for (Result result : resultScaner) {
-            String str = Bytes.toString(result.getValue(Bytes.toBytes("info"),
-                    Bytes.toBytes("money")));
-            System.out.println(str);
-        }
-        table.close();
-    }
 
 }
