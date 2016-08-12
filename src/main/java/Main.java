c0d9486d06ba53575801ca531f10499fcb6aa8d9
boolyahoo <boolyahoo@gmail.com>
2016-08-12 20:13:38 +0800

    一个博客存储的例子

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.client.Result;
 
@@ -13,24 +14,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String... args) throws IOException {
-        // 1.初始化HBaseOperation
         Configuration config = new Configuration();
-        config.set("hbase.zookeeper.quorum", "H01,H02,H03");
-        config.set("hbase.zookeeper.property.clientPort", "2181");
+        config.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"));
+        config.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"));
         HBaseOperation hbase = new HBaseOperation(config);
 
         // 2. 测试相应操作
         // 2.1 创建表
         String tableName = "blog";
         String colFamilies[] = {"article", "author"};
-        hbase.createTable(tableName, colFamilies);
+        //hbase.createTable(tableName, colFamilies);
 
-        // 2.2 插入一条记录
-        hbase.insertRecord(tableName, "row1", "article", "title", "hadoop");
-        hbase.insertRecord(tableName, "row1", "author", "name", "tom");
-        hbase.insertRecord(tableName, "row1", "author", "nickname", "tt");
+        hbase.insertRecord(tableName, "row2", "article", "title", "hadoop");
+        hbase.insertRecord(tableName, "row2", "author", "name", "tom");
+        hbase.insertRecord(tableName, "row2", "author", "nickname", "tt");
 
-        // 2.2 查询一条记录
         Result rs1 = hbase.getRecord(tableName, "row1");
         for (Cell cell : rs1.rawCells()) {
             System.out.println(new String(cell.getRowArray()));
@@ -39,21 +37,16 @@ public class Main {
             System.out.println(new String(cell.getValueArray()));
         }
 
-        // 2.3 查询所有记录
         List<Result> list = hbase.getAllRecord(tableName);
         Iterator<Result> it = list.iterator();
         while (it.hasNext()) {
             Result rs2 = it.next();
             for (Cell cell : rs2.rawCells()) {
-                System.out.println("row key is : "
-                        + new String(cell.getRowArray()));
-                System.out.println("family is  : "
-                        + new String(cell.getFamilyArray()));
-                System.out.println("qualifier is:"
-                        + new String(cell.getQualifierArray()));
+                System.out.println("row key is : " + new String(cell.getRowArray()));
+                System.out.println("family is  : " + new String(cell.getFamilyArray()));
+                System.out.println("qualifier is:" + new String(cell.getQualifierArray()));
                 System.out.print("timestamp is:" + cell.getTimestamp());
-                System.out.println("Value  is  : "
-                        + new String(cell.getValueArray()));
+                System.out.println("Value  is  : " + new String(cell.getValueArray()));
             }
         }
 
